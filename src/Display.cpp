96c2f04cf7038580c1d84b2be75db643c7003869
Igor Hock <igor.hock@hagergroup.com>
2026-07-08 16:00:41 +0200

    display 'IT IS' and extra minutes if enabled in settings

--- src/Display.cpp
+++ src/Display.cpp
@@ -16,7 +16,7 @@
 
 
 /* Log level for this module */
-#define LOG_LEVEL   (LOG_DEBUG)
+#define LOG_LEVEL   (LOG_VERBOSE)
 
 
 /* Delay in msec between display updates */
@@ -281,25 +281,42 @@ void Display::PaintTime(const uint8_t aHour, const uint8_t aMinute, const CRGB a
             wHour = 0;
         }
 
+        /* Check if "IT IS" words should be displayed */
+        bool  wItIsEnabled = Settings.GetValue<bool>(ConfigNS::mKeyDisplayClockItIs, ConfigNS::mDefaultDisplayClockItIs);
+        uint8_t wItIsWords = (wItIsEnabled) ? 2 : 0; // 2 words: "ES" and "IST"
+
+        /* Check if extra minutes should be displayed */
+        bool  wExtraMinutesEnabled = Settings.GetValue<bool>(ConfigNS::mKeyDisplayClockSingleMins, ConfigNS::mDefaultDisplayClockSingleMins);
+        uint8_t wExtraMinutesWords = (wExtraMinutesEnabled) ? MAX_EXTRA_MINUTE_WORDS : 0;
+
         /* Prepare array of words to display the time */
-        tWord wDisplayWords[MAX_MINUTE_WORDS + MAX_HOUR_WORDS + MAX_EXTRA_MINUTE_WORDS + 2] = { WORD_END_OF_WORDS }; // +2 for "ES" and "IST" words
+        tWord wDisplayWords[wItIsWords + MAX_MINUTE_WORDS + MAX_HOUR_WORDS + wExtraMinutesWords] = { WORD_END_OF_WORDS };
         uint8_t wWordsOffset = 0;
 
-        /* Add "ES IST" words */
-        wDisplayWords[wWordsOffset++] = WORD_ES;
-        wDisplayWords[wWordsOffset++] = WORD_IST;
+        /* Add "ES IST" words if enabled */
+        if (wItIsEnabled)
+        {
+            wDisplayWords[wWordsOffset++] = WORD_ES;
+            wDisplayWords[wWordsOffset++] = WORD_IST;
+        }
+
         /* Add minute words */
         memccpy(&wDisplayWords[wWordsOffset], wMinuteDisplay.wMinuteWords,
             MAX_MINUTE_WORDS, sizeof(wMinuteDisplay.wMinuteWords));
         wWordsOffset += MAX_MINUTE_WORDS;
+
         /* Add hour words */
         memccpy(&wDisplayWords[wWordsOffset], mcWordHoursTable[wMinuteDisplay.mHourMode][wHour],
             MAX_HOUR_WORDS, sizeof(mcWordHoursTable[wMinuteDisplay.mHourMode][wHour]));
         wWordsOffset += MAX_HOUR_WORDS;
-        /* Add extra minute words */
-        memccpy(&wDisplayWords[wWordsOffset], mcWordExtraMinutesTable[wMinuteExtra],
-            MAX_EXTRA_MINUTE_WORDS, sizeof(mcWordExtraMinutesTable[wMinuteExtra]));
-        wWordsOffset += MAX_EXTRA_MINUTE_WORDS;
+
+        /* Add extra minute words if enabled */
+        if (wExtraMinutesEnabled)
+        {
+            memccpy(&wDisplayWords[wWordsOffset], mcWordExtraMinutesTable[wMinuteExtra],
+                wExtraMinutesWords, sizeof(mcWordExtraMinutesTable[wMinuteExtra]));
+            wWordsOffset += wExtraMinutesWords;
+        }
 
         /* Set LED bits for all display words */
         for (uint8_t wI = 0; wI < wWordsOffset; wI++)
